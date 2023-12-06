e405ef27e4a2e245ee5d2fe7327484c2dc030f5a
Hojun <hojun2584@gmail.com>
2023-12-06 19:20:39 +0900

    Revert "231206_jaeyoung_hojun_preMerge"
    This reverts commit 5122bea1962ac955c6e946e62125148a8af80bdc.

--- Assets/Script/Character/StateMachine.cs
+++ Assets/Script/Character/StateMachine.cs
@@ -8,16 +8,7 @@ namespace Hojun
     public interface IStateMachine
     {
         object GetOwner();
-        void SetState(ZombieState stateName);
-    }
-
-
-    public enum ZombieState
-    {
-        IDLE,
-        SEARCH_WALK,
-        SEARCH_RUN,
-        FIND
+        void SetState(MoveState stateName);
     }
 
     public enum MoveState
@@ -32,34 +23,22 @@ namespace Hojun
     {
         public T owner = null;
         public State curState;
-<<<<<<< Updated upstream
         public Dictionary<MoveState, State> stateDic;
-=======
-
->>>>>>> Stashed changes
 
         public StateMachine(T owner)
         {
             this.owner = owner;
-<<<<<<< Updated upstream
             stateDic = new Dictionary<MoveState, State>();
-=======
->>>>>>> Stashed changes
         }
 
-        public void AddState(ZombieState stateName, State state)
+        public void AddState(MoveState stateName, State state)
         {
-<<<<<<< Updated upstream
             if (stateDic.ContainsKey(stateName))
                 return;
 
             stateDic.Add(stateName, state);
             state.Init(this);
 
-=======
-
-        
->>>>>>> Stashed changes
         }
 
         public object GetOwner()
@@ -67,7 +46,6 @@ namespace Hojun
             return owner;
         }
 
-<<<<<<< Updated upstream
         public void SetState(MoveState stateName)
         {
             if (stateDic.ContainsKey(stateName))
@@ -80,19 +58,6 @@ namespace Hojun
                 curState.Enter();
 
             }
-=======
-        public void SetState(Hojun.ZombieState stateName)
-        {
-        //    if (moveDic.ContainsKey(stateName))
-        //    {
-        //        if (curState != null)
-        //        {
-        //            curState.Exit();
-        //        }
-        //        curState = moveDic[stateName];
-        //        curState.Enter();
-        //    }
->>>>>>> Stashed changes
         }
 
         public void Update()
