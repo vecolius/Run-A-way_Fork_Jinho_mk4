b1a281b732c2629a35f5c85aab39726236c467ec
doridori238 <153061670+doridori238@users.noreply.github.com>
2023-12-06 20:41:54 +0900

    씬전환?
    아직 안끝남 ㅜ

--- Assets/Script/Manager/SceneController.cs
+++ Assets/Script/Manager/SceneController.cs
@@ -2,13 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using Unity.VisualScripting;
 
 public class SceneController : DontDestroySingle<SceneController>
 {
-
+    [SerializeField] protected GameObject titleUi;
+    [SerializeField] protected GameObject mainUi;
+    [SerializeField] protected GameObject lobbyUi;
+    [SerializeField] protected GameObject gameUi;
+    [SerializeField] protected GameObject loadingUi;
+    // [SerializeField] private GameObject lobbyMultiUi; //  멀티 일 때 사용 예정
     private new void Awake()
     {
         base.Awake();
+    }
+
+   
+    private void Start()
+    {
+        SceneController.LoadScene("TitleScene");
+       
+    }
+
+    SceneController()
+    {
+        titleUi = GetComponent<SceneController>().titleUi;
+        mainUi = GetComponent<SceneController>().mainUi;
+        lobbyUi = GetComponent<SceneController>().lobbyUi;
+        gameUi = GetComponent<SceneController>().gameUi;
+        loadingUi = GetComponent<SceneController>().loadingUi;
+    }
+
+
+    public void ChangeScene(string sceneName)
+    {
+
+        switch (sceneName)
+        {
+            case "TitleScene":
+                titleUi.active = true;
+                break;
+            case "MainScene":
+                mainUi.active = true;
+                break;
+            case "LobbySceneSingle":
+                lobbyUi.active = true;
+                break;
+            case "GameScene":
+                gameUi.active = true;
+                break;
+            case "LoadingScene":
+                loadingUi.active = true;
+                break;
+            //case "LobbyMultiScene":
+            //  멀티 일 때 사용 예정
+            //    break;
+        }
+
     }
 
 
@@ -16,6 +66,11 @@ public class SceneController : DontDestroySingle<SceneController>
     public static void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    private void Update()
+    {
+        ChangeScene("sceneName");
     }
 
 
