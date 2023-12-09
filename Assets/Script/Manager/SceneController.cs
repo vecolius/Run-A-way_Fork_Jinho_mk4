1a417058c6de576a6853d0059ef073ad7035b63c
doridori238! <dkffps238@gmail.com>
2023-12-09 16:49:43 +0900

    약간 수정

--- Assets/Script/Manager/SceneController.cs
+++ Assets/Script/Manager/SceneController.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 using Unity.VisualScripting;
 using UnityEngine.UI;
 
-// 전체적으로 UI는 조금씩 계속 만들어주는 방향으로 가야할 듯 하다.
+
 // 플레이어 공격 부분도 같이 만들어야 하기 떄문에 구조 생각하고 연결 가능하게 만들어서 
-// 다른 곳 도 연결해야 하니까 웬만하면 [SerializeField]를 이용하자 연결하기는 이게 쉬움!
+
 public class SceneController : DontDestroySingle<SceneController>
 {
     [SerializeField] GameObject titleUi;
@@ -16,6 +16,7 @@ public class SceneController : DontDestroySingle<SceneController>
     [SerializeField] GameObject gameUi;
     [SerializeField] GameObject loadingUi;
 
+    //[SerializeField] G
 
     List<GameObject> totalUi= new List<GameObject>(); 
 
@@ -29,14 +30,7 @@ public class SceneController : DontDestroySingle<SceneController>
     // 사용해도 됨 그게 좀 더 간단 하긴 함.
     public void Option(string optionUi) // 메인 화면 옵션
     {
-        // 일단 가져온 optionUi를 끄고 키기 원하는데
-        // 그걸 위해서는 다른 방식을 이용해 부르거나 Onclick 함수를 사용해
-        // 보는 것도 방법 일 듯 하다 
-        // 다른 방식은 밑에서 시도 하던 방법으로 해봐도 나쁘지 않을 듯 함.
-        // 연결만 잘 하면 돌아갈지도?
-        // 참고로 옵션 부분은 사운드 부분도 들어가고 하기 때문에 좀 더 고민하고 해보기.
-
-
+        
         //OptionImage
         //Button option;
         //option = mainUi.GetComponentInChildren<Button>();
