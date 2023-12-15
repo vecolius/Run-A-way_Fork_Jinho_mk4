525aa872393971f5ece47dfb97202057473ef4ee
doridori238 <153061670+doridori238@users.noreply.github.com>
2023-12-15 21:32:49 +0900

    로딩씬 추가됨
    아직 정확히 연결 되지 않아서 오류납니다

---
+++ Assets/Gayoung/SceneManager/Loading.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class Loading : MonoBehaviour
+{
+
+    public Slider progressBar;
+    public Image barColor;
+    public TextMeshProUGUI loadtext;
+    //private float completeTime = 1f;
+    //private float uncompleteTime = 0.9f;
+    public string sceneName;
+    public void Awake()
+    {
+        sceneName = SceneController.nextSceneName;
+    }
+    private void Start()
+    {
+        StartCoroutine(LoadScene());
+    }
+
+    IEnumerator LoadScene()
+    {
+        yield return null;
+        // LoadSceneAsync() 비동기로드.
+        // Scene을 불러오면 완료까지 다른 작업을 수행하지 않음.
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+        // 로딩이 끝나도 멈춤.
+        operation.allowSceneActivation = false;
+
+
+        while (!operation.isDone)
+        {
+            yield return null;
+
+            //Color sliderColor = Color.Lerp(Color.white, Color.blue, completeTime);
+            //barColor.color = sliderColor;
+
+            if (progressBar.value < 0.9f)
+            {
+                progressBar.value = Mathf.MoveTowards(progressBar.value, 0.9f, Time.deltaTime);
+            }
+            else if (operation.progress >= 0.9f)
+            {
+                progressBar.value = Mathf.MoveTowards(progressBar.value, 1f, Time.deltaTime);
+            }
+
+            if (progressBar.value >= 1f)
+                loadtext.text = "Press SpaceBar";
+
+            if (Input.GetKeyDown(KeyCode.Space) &&
+                progressBar.value >= 1f && operation.progress >= 0.9f)
+            {
+                operation.allowSceneActivation = true;
+            }
+        }
+    }
+}
--- Assets/Gayoung/SceneManager/SceneController.cs
+++ Assets/Gayoung/SceneManager/SceneController.cs
@@ -26,8 +26,8 @@ public class SceneController : DontDestroySingle<SceneController>
     [SerializeField] GameObject lobbyProfessionChoiceButton;
 
 
-    List<GameObject> totalUi= new List<GameObject>(); 
-
+    List<GameObject> totalUi= new List<GameObject>();
+    public static string nextSceneName;
     // [SerializeField] private GameObject lobbyMultiUi; //  ��Ƽ �� �� ��� ����
     private new void Awake()
     {
@@ -113,6 +113,7 @@ public class SceneController : DontDestroySingle<SceneController>
     //button click component [SerializeField] use
     public static void LoadScene(string sceneName)
     {
+        nextSceneName = sceneName;
         SceneManager.LoadScene(sceneName);
         instance.ChangeScene(sceneName);
    
